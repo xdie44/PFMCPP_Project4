103224cf6a033aeeb606ac8d8f02a4b77eaf5142
xdie44 <diego.capoccitti@gmail.com>
2020-09-13 01:52:25 +0000

    Finished Part1

--- main.cpp
+++ main.cpp
@@ -92,7 +92,119 @@ good to go!
  */
 
 #include <iostream>
+
+struct FloatType 
+{
+    float add( float lhs, float rhs);
+    float subtract(float lhs, float rhs);
+    float multiply(float lhs, float rhs);
+    float divide(float lhs, float rhs);
+};
+
+float FloatType::add(float lhs, float rhs)
+{
+    float res = lhs + rhs;
+    return res;
+}
+
+float FloatType::subtract(float lhs, float rhs)
+{
+    float res = lhs - rhs;
+    return res;
+}
+
+float FloatType::multiply (float lhs, float rhs)
+{
+    float res = lhs * rhs;
+    return res;
+}
+
+float FloatType::divide (float lhs, float rhs)
+{
+    if (rhs == 0.0f)
+    { 
+        std::cout << std::endl << "warning, floating point division by zero returns 'inf' !" << std::endl; 
+    }
+    float res = lhs / rhs;
+    return res;
+}
+
+struct DoubleType 
+{
+    double add( double lhs, double rhs);
+    double subtract(double lhs, double rhs);
+    double multiply(double lhs, double rhs);
+    double divide(double lhs, double rhs);
+};
+
+double DoubleType::add(double lhs, double rhs)
+{
+    double res = lhs + rhs;
+    return res;
+}
+
+double DoubleType::subtract(double lhs, double rhs)
+{
+    double res = lhs - rhs;
+    return res;
+}
+
+double DoubleType::multiply (double lhs, double rhs)
+{
+    double res = lhs * rhs;
+    return res;
+}
+
+double DoubleType::divide (double lhs, double rhs)
+{
+    if (rhs == 0.0)
+    { 
+        std::cout << std::endl << "warning, floating point division by zero returns 'inf' !" << std::endl; 
+    } 
+    double res = lhs / rhs;
+    return res;
+}
+
+struct IntType 
+{
+    int add( int lhs, int rhs);
+    int subtract(int lhs, int rhs);
+    int multiply(int lhs, int rhs);
+    int divide(int lhs, int rhs);
+};
+
+int IntType::add(int lhs, int rhs)
+{
+    int res = lhs + rhs;
+    return res;
+}
+
+int IntType::subtract(int lhs, int rhs)
+{
+    int res = lhs - rhs;
+    return res;
+}
+
+int IntType::multiply (int lhs, int rhs)
+{
+    int res = lhs * rhs;
+    return res;
+}
+
+int IntType::divide (int lhs, int rhs)
+{
+    if (rhs == 0.0)
+    { 
+        std::cout << "error, integer division by zero will crash the program!" << std::endl;
+        std::cout << "returning lhs" << std::endl;
+        return lhs;
+    } 
+    int res = lhs / rhs;
+    return res;
+}
+
 int main() 
+
 {
     FloatType ft;
     std::cout << "result of ft.add(): " << ft.add( 123.456f, 432.1f) << std::endl;
