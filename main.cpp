f9b6141382b26f36936725d48dad6b96b27a6a51
xdie44 <diego.capoccitti@gmail.com>
2020-10-14 17:16:05 +0000

    Step2

--- main.cpp
+++ main.cpp
@@ -24,6 +24,8 @@ Create a branch named Part8
  */
 
 #include <typeinfo>
+#include <iostream>
+
 template<typename NumericType>
 struct Temporary
 {
@@ -47,7 +49,7 @@ private:
  2) add the definition of Temporary::counter here, which is a static variable and must be defined outside of the class.
     Remember the rules about how to define a Template member variable/function outside of the class.
 */
-
+int Temporary::counter {0};
 /*
  3) You'll need to template your overloaded math operator functions in your Templated Class from Ch5 p04
     use static_cast to convert whatever type is passed in to your template's NumericType before performing the +=, -=, etc.  here's an example implementation:
